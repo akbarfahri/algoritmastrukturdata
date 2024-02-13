f749dd6eedd71c4a5e9054701afa40c4f1cee161
akbar fahri <144417878+akbarfahri@users.noreply.github.com>
2024-02-13 21:47:09 +0700

    Update deretbilangan.java

--- deretbilangan.java
+++ deretbilangan.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+
+public class deretBilangan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai n: ");
+        double n = scanner.nextLong();
+
+        // Mengambil dua digit terakhir dari n
+        int twoDigitN = (int) (n % 100);
+
+        generateSequence(twoDigitN);
+
+        scanner.close();
+    }
+
+    static void generateSequence(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i == 6 || i == 10) {
+                System.out.print("x ");
+            } else if (i % 2 != 0) {
+                System.out.print("* ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
 
